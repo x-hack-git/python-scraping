4277b7f450aaf37068daf98942a7ac50e30ada69
Hidehiro Nagaoka <kuinige@gmail.com>
2018-11-09 18:51:00 +0900

    Added the function to write the csv

--- scraping2.py
+++ scraping2.py
@@ -2,6 +2,7 @@
 import urllib.request as req
 from bs4 import BeautifulSoup
 import time
+import csv
 
 # スクレイピングするURLを設定
 url = "https://www.amazon.co.jp/s/ref=nb_sb_noss_2?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&url=search-alias%3Daps&field-keywords=python"
@@ -13,8 +14,10 @@ webpage = req.urlopen(url)
 soup = BeautifulSoup(webpage, 'html.parser')
 results_array = soup.select(".s-access-title")
 
-titles_array = []
-for result in results_array:
-    title = result.string
-    print(title)
-    result.append(title)
+with open('results.csv', 'w') as f:
+    writer = csv.writer(f, lineterminator='\n') # 改行コード（\n）を指定しておく
+
+    for result in results_array:
+        title = result.string
+        print(title)
+        writer.writerow([title])
